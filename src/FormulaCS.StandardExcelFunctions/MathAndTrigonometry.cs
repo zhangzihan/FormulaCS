21759f6223e59fd5209a790dc84573679bfc2c40
Radu Florescu <Radu.Florescu@hotmail.com>
2018-05-22 10:20:38 +0100

    Added ability to apply weighting

--- src/FormulaCS.StandardExcelFunctions/MathAndTrigonometry.cs
+++ src/FormulaCS.StandardExcelFunctions/MathAndTrigonometry.cs
@@ -25,6 +25,7 @@ namespace FormulaCS.StandardExcelFunctions
                 {"SUM", SumFunction},
                 {"RANGE", RangeFunction},
                 {"STDEV", StdevFunction},
+                {"WEIGHTED", WeightedFunction},
             };
         }
 
@@ -403,6 +404,54 @@ namespace FormulaCS.StandardExcelFunctions
             }
 
             args.Result = Math.Round(n);
+        }
+
+        private static void WeightedFunction(IFunctionArgs args, IExcelCaller caller)
+        {
+            if (args.Parameters.Length != 1 && args.Parameters.Length != 2)
+            {
+                throw new ArgumentException(
+                    $"WEIGHTED function takes 1 or 2 arguments, got {args.Parameters.Length}",
+                    nameof(args));
+            }
+
+            var arg1 = args.Parameters[0].Evaluate();
+            if (arg1 is ErrorValue)
+            {
+                args.Result = arg1;
+                return;
+            }
+            var arg2 = new object();
+
+            if (args.Parameters.Length == 2)
+            {
+                arg2 = args.Parameters[1].Evaluate();
+                if (arg2 is ErrorValue)
+                {
+                    args.Result = arg2;
+                    return;
+                }
+            }
+            else
+            {
+                arg2 = 1;
+            }
+
+            var val1 = Conversion.ToDoubleOrErrorValue(arg1);
+            if (val1 is ErrorValue)
+            {
+                args.Result = val1;
+                return;
+            }
+
+            var val2 = Conversion.ToDoubleOrErrorValue(arg2);
+            if (val2 is ErrorValue)
+            {
+                args.Result = val2;
+                return;
+            }
+
+            args.Result = (double)val1 * (double)val2;
         }
 
         private static void RoundUpFunction(IFunctionArgs args, IExcelCaller caller)
